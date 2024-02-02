08d9872a9e3dbaaafb0656bc05c7de01117d7813
Valam <FranciscoValamCA@gmail.com>
2024-02-02 17:32:36 -0600

    primer commit

---
+++ data/diccionarios.py
@@ -0,0 +1,246 @@
+categoria_alimento = {
+    'Vegetables': 'Vegetales',
+    'Snack Foods': 'Alimentos para picar',
+    'Dairy': 'Productos lácteos',
+    'Meat': 'Carne',
+    'Fruit': 'Fruta',
+    'Jams and Jellies': 'Mermeladas y jaleas',
+    'Baking Goods': 'Productos para hornear',
+    'Breakfast Foods': 'Alimentos para el desayuno',
+    'Bread': 'Pan',
+    'Canned Soup': 'Sopa enlatada',
+    'Beer and Wine': 'Cerveza y vino',
+    'Paper Products': 'Productos de papel',
+    'Bathroom Products': 'Productos de baño',
+    'Electrical': 'Eléctrico',
+    'Candy': 'Dulces',
+    'Frozen Desserts': 'Postres congelados',
+    'Specialty': 'Especialidades',
+    'Starchy Foods': 'Alimentos ricos en almidón',
+    'Magazines': 'Revistas',
+    'Kitchen Products': 'Productos de cocina',
+    'Eggs': 'Huevos',
+    'Hot Beverages': 'Bebidas calientes',
+    'Hygiene': 'Higiene',
+    'Pain Relievers': 'Analgésicos',
+    'Pizza': 'Pizza',
+    'Carbonated Beverages': 'Bebidas carbonatadas',
+    'Pure Juice Beverages': 'Bebidas de jugo puro',
+    'Cleaning Supplies': 'Suministros de limpieza',
+    'Side Dishes': 'Acompañamientos',
+    'Frozen Entrees': 'Platos preparados congelados',
+    'Drinks': 'Bebidas',
+    'Plastic Products': 'Productos de plástico',
+    'Hardware': 'Artículos de ferretería',
+    'Cold Remedies': 'Remedios para el resfriado',
+    'Decongestants': 'Descongestionantes',
+    'Canned Tuna': 'Atún enlatado',
+    'Seafood': 'Mariscos',
+    'Canned Anchovies': 'Anchoas enlatadas',
+    'Canned Clams': 'Almejas enlatadas',
+    'Candles': 'Velas',
+    'Miscellaneous': 'Misceláneos',
+    'Canned Oysters': 'Ostras enlatadas',
+    'Canned Shrimp': 'Camarones enlatados',
+    'Packaged Vegetables': 'Vegetales envasados',
+    'Canned Sardines': 'Sardinas enlatadas'
+}
+
+departamento = {
+    'Produce': 'Productos frescos',
+    'Snack Foods': 'Alimentos para picar',
+    'Household': 'Artículos para el hogar',
+    'Frozen Foods': 'Alimentos congelados',
+    'Baking Goods': 'Productos para hornear',
+    'Canned Foods': 'Alimentos enlatados',
+    'Dairy': 'Productos lácteos',
+    'Health and Hygiene': 'Salud e higiene',
+    'Beverages': 'Bebidas',
+    'Deli': 'Charcutería',
+    'Baked Goods': 'Productos horneados',
+    'Alcoholic Beverages': 'Bebidas alcohólicas',
+    'Snacks': 'Snacks',
+    'Starchy Foods': 'Alimentos ricos en almidón',
+    'Periodicals': 'Publicaciones periódicas',
+    'Eggs': 'Huevos',
+    'Breakfast Foods': 'Alimentos para el desayuno',
+    'Canned Products': 'Productos enlatados',
+    'Checkout': 'Caja de salida',
+    'Meat': 'Carne',
+    'Seafood': 'Mariscos',
+    'Carousel': 'Carrusel'
+}
+
+tipo= {'Food':'Comida', 'Non-Consumable':'No comestible', 'Drink':'Bebida'}
+
+promocion = {
+    'Weekend Markdown': 'Descuento de fin de semana',
+    'Two Day Sale': 'Venta de dos días',
+    'Price Savers': 'Ahorro de precios',
+    'Price Winners': 'Ganadores de precios',
+    'Save-It Sale': 'Venta Ahorra Ya',
+    'Super Duper Savers': 'Super Ahorradores',
+    'Super Savers': 'Super Ahorro',
+    'One Day Sale': 'Venta de un día',
+    'Double Down Sale': 'Venta Doble',
+    'High Roller Savings': 'Ahorro para apostadores grandes',
+    'Green Light Days': 'Días de luz verde',
+    'Shelf Clearing Days': 'Días de liquidación de estanterías',
+    'Price Slashers': 'Rebajadores de precios',
+    'Free For All': 'Gratis para todos',
+    'Cash Register Lottery': 'Lotería de la caja registradora',
+    'Go For It': 'Adelante',
+    'Two for One': 'Dos por uno',
+    'Money Savers': 'Ahorradores de dinero',
+    'Big Time Discounts': 'Descuentos de gran magnitud',
+    'Price Destroyers': 'Destructores de precios',
+    'Saving Days': 'Días de ahorro',
+    'Dollar Cutters': 'Recortadores de dólares',
+    'Unbeatable Price Savers': 'Ahorro de precios imbatible',
+    'Sales Galore': 'Promociones por montones',
+    'Big Promo': 'Gran promoción',
+    'Dimes Off': 'Más barato',
+    'Sales Days': 'Días de venta',
+    'Price Cutters': 'Rebajando precios',
+    'You Save Days': 'Días de ahorro',
+    'Savings Galore': 'Ahorros por montones',
+    'Bag Stuffers': 'Bolsas llenas',
+    'Three for One': 'Tres por uno',
+    'I Cant Believe It Sale': 'Venta No Puedo Creerlo',
+    'Sale Winners': 'Ganadores de ventas',
+    'Pick Your Savings': 'Elige tus ahorros',
+    'Bye Bye Baby': 'Adiós bebé',
+    'Best Savings': 'Mejores ahorros',
+    'Tip Top Savings': 'Ahorro de primera clase',
+    'Shelf Emptiers': 'Estantes vacíos',
+    'Big Time Savings': 'Ahorro Enorme',
+    'Wallet Savers': 'Ahorradores de billetera',
+    'Dollar Days': 'Días de dólar',
+    'Mystery Sale': 'Venta misteriosa',
+    'Price Smashers': 'Aplastadores de precios',
+    'Green Light Special': 'Especial luz verde',
+    'Coupon Spectacular': 'Espectacular de cupones',
+    'Super Wallet Savers': 'Super ahorradores de billetera',
+    'Double Your Savings': 'Duplica tus ahorros',
+    'Fantastic Discounts': 'Descuentos fantásticos'
+}
+
+escolaridad = {
+    'Partial High School':'Primaria', 
+    'High School Degree':'Secundaria', 
+    'Bachelors Degree':'Superior', 
+    'Partial College':'Técnico', 
+    'Graduate Degree':'Maestría'
+}
+
+miembro = {
+    'Bronze':'Bronce', 
+    'Golden': 'Oro', 
+    'Silver':'Plata'
+}
+
+ocupacion = {
+    'Professional':'Profesional', 
+    'Skilled Manual':'Técnico', 
+    'Manual':'Obrero', 
+    'Management':'Gestión', 
+    'Clerical':'Cargo de Oficina'
+}
+
+pais = {
+    'USA':'Estados Unidos', 
+    'Mexico':'México', 
+    'Canada':'Canadá'
+}
+
+estado_civil = {
+    'S':'Soltero', 'M':'Casado'
+}
+
+genero ={
+    'F':'Femenino', 
+    'M':'Masculino'
+}
+
+vivienda_propia={'Y':'Sí', 'N':'No'}
+
+ingreso_anual={
+    '$30K - $50K':40000, 
+    '$10K - $30K':20000, 
+    '$50K - $70K':60000, 
+    '$70K - $90K':80000, 
+    '$130K - $150K':140000, 
+    '$90K - $110K':100000, 
+    '$110K - $130K':120000, 
+    '$150K +':160000
+}
+
+
+
+comercio = {
+    'Supermarket': 'Supermercado',
+    'Deluxe Supermarket': 'Supermercado de lujo',
+    'Gourmet Supermarket': 'Supermercado gourmet',
+    'Mid-Size Grocery': 'Supermercado mediano',
+    'Small Grocery': 'Pequeño supermercado'
+}
+
+
+divulgacion = {
+    'Daily Paper, Radio': 'Periódico diario, radio',
+    'Product Attachment': 'Anexo del producto',
+    'Daily Paper, Radio, TV': 'Periódico diario, radio, televisión',
+    'Daily Paper': 'Periódico diario',
+    'Street Handout': 'Panfletos',
+    'Radio': 'Radio',
+    'Sunday Paper': 'Periódico dominical',
+    'In-Store Coupon': 'Cupón en la tienda',
+    'Sunday Paper, Radio': 'Periódico dominical, radio',
+    'Cash Register Handout': 'Entrega en el punto de venta',
+    'TV': 'Televisión',
+    'Bulk Mail': 'Correo masivo',
+    'Sunday Paper, Radio, TV': 'Periódico dominical, radio, televisión'
+}
+
+columnas = {
+    'food_category': 'categoria_alimento',
+    'food_department': 'departamento',
+    'food_family': 'tipo',
+    'store_sales(in millions)': 'ventas_tienda(en millones)',
+    'store_cost(in millions)': 'costo_tienda(en millones)',
+    'unit_sales(in millions)': 'ventas_unitarias(en millones)',
+    'promotion_name': 'promocion',
+    'sales_country': 'pais',
+    'marital_status': 'estado_civil',
+    'gender': 'genero',
+    'total_children': 'total_de_hijos',
+    'education': 'escolaridad',
+    'member_card': 'miembro',
+    'occupation': 'ocupacion',
+    'houseowner': 'vivienda_propia',
+    'avg_cars_at home(approx)': 'promedio_autos(aprox)',
+    'avg. yearly_income': 'ingreso_anual',
+    'num_children_at_home': 'numero_hijos',
+    'avg_cars_at home(approx).1': 'promedio_autos_2_(aprox)',
+    'brand_name': 'marca',
+    'SRP': 'precio_recomendado',
+    'gross_weight': 'peso_bruto',
+    'net_weight': 'peso_neto',
+    'recyclable_package': 'empaque_reciclable',
+    'low_fat': 'bajo_en_grasa',
+    'units_per_case': 'unidades_por_caja',
+    'store_type': 'comercio',
+    'store_city': 'ciudad',
+    'store_state': 'estado',
+    'store_sqft': 'pies_cuadrados_tienda',
+    'grocery_sqft': 'pies_cuadrados_comestibles',
+    'frozen_sqft': 'pies_cuadrados_congelados',
+    'meat_sqft': 'pies_cuadrados_carne',
+    'coffee_bar': 'cafeteria',
+    'video_store': 'tienda_de_videos',
+    'salad_bar': 'barra_de_ensaladas',
+    'prepared_food': 'comida_preparada',
+    'florist': 'floristeria',
+    'media_type': 'divulgacion',
+    'cost': 'costo'
+}
